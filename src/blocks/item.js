6dd3abab8ac23a64f951e56021872c11ee52aee2
Nick Trunov <trunov1997@yandex.ru>
2020-08-25 23:27:30 +0300

    Added fixes for buy button

--- src/blocks/item.js
+++ src/blocks/item.js
@@ -4,15 +4,15 @@ $('.js-card__body').on('click', function(){
     $(this).addClass('card__body--no-hover');
 
     $(this).siblings('.card__bottom').children('.card__bottom-text').toggleClass('card__bottom-text--inactive');
-    $(this).siblings('.card__bottom').children('.card__bottom-link').toggleClass('card__bottom-link--inactive');
+    $(this).siblings('.card__bottom').children('.card__bottom-buy').toggleClass('card__bottom-buy--inactive');
 });
 
-$('.js-card__link').on('click', function () {
+$('.js-card__buy-button').on('click', function () {
     $(this).closest('.card__bottom').siblings('.js-card__body').toggleClass('card__body--inactive');
     $(this).closest('.card__bottom').siblings('.js-card__body').toggleClass('card__body--active');
 
-    $(this).closest('.card__bottom-link').siblings('.card__bottom-text').toggleClass('card__bottom-text--inactive');
-    $(this).closest('.card__bottom-link').toggleClass('card__bottom-link--inactive');
+    $(this).closest('.card__bottom-buy').siblings('.card__bottom-text').toggleClass('card__bottom-text--inactive');
+    $(this).closest('.card__bottom-buy').toggleClass('card__bottom-buy--inactive');
 });
 
 $('.js-card__body').on('mouseleave', function () {
